845409e8e953a4596d667be24fbd3fa15a781049
Lã Xuân Linh <laxuanlong@gmail.com>
2018-12-15 16:36:04 +0700

    Add jwt

--- src/main/java/com/example/authclient/AuthClientApplication.java
+++ src/main/java/com/example/authclient/AuthClientApplication.java
@@ -1,16 +1,61 @@
 package com.example.authclient;
 
+import io.micrometer.core.instrument.util.IOUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.io.IOException;
 
 
 @EnableResourceServer
 @SpringBootApplication
-public class AuthClientApplication {
+public class AuthClientApplication extends ResourceServerConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthClientApplication.class, args);
 	}
 
+	@Override
+	public void configure(ResourceServerSecurityConfigurer config) {
+		config.tokenServices(tokenServices());
+	}
+
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		Resource resource = new ClassPathResource("public.txt");
+		String publicKey = null;
+		try{
+			publicKey = IOUtils.toString(resource.getInputStream());
+
+		} catch (final IOException e){
+			throw new RuntimeException();
+		}
+		converter.setVerifierKey(publicKey);
+		return converter;
+	}
+
+	@Bean
+	@Primary
+	public DefaultTokenServices tokenServices() {
+		DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+		defaultTokenServices.setTokenStore(tokenStore());
+		return defaultTokenServices;
+	}
 }
--- src/main/java/com/example/authclient/SecurityConfig.java
+++ src/main/java/com/example/authclient/SecurityConfig.java
@@ -1,10 +1,8 @@
 package com.example.authclient;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
